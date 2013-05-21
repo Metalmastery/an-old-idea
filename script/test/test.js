1062e4c9d939414268b03e63415ac20134768d76
Metalmastery <metalmastery@gmail.com>
2013-05-21 22:53:33 +0300

    isometric tests

---
+++ script/test/test.js
@@ -0,0 +1,75 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: Медведь
+ * Date: 19.05.13
+ * Time: 8:49
+ * To change this template use File | Settings | File Templates.
+ */
+(function(){
+    var cl = function(){
+        console.log('callback');
+        SES.send('another');
+    };
+
+    var cl2 = function(){
+        console.log('another callback');
+    };
+
+    SES.connect('spawn', function(){
+        Factory('Objects', 'baseItem')
+    });
+
+    SES.connect('created', function(){
+        console.log(arguments);
+        document.getElementById('clickable').innerHTML = JSON.stringify(arguments[0], '', 4);
+    });
+
+    var size = 500;
+    var count = 5;
+    var cell = size / count / 2;
+
+    var canva = document.createElement('canvas');
+        canva.width = size;
+        canva.height = size;
+    var holder = document.getElementById('clickable');
+    var d = document.getElementById('display');
+    holder.appendChild(canva);
+
+    var ctx = canva.getContext('2d');
+
+    for (var x = 0; x < count+1; x++){
+        var startPoint = Utils.isometricToScreen({'x' : 0, 'y' :  x * cell});
+        var endPoint = Utils.isometricToScreen({'x' : count * cell, 'y' :  x * cell});
+
+        var xStart = size / 2 + startPoint.x;
+        var yStart = size / 2 + startPoint.y;
+
+        var xEnd = size / 2 + endPoint.x;
+        var yEnd = size / 2 + endPoint.y;
+
+        ctx.moveTo(xStart, yStart);
+        ctx.lineTo(xEnd, yEnd);
+
+        var startPoint = Utils.isometricToScreen({'x' : x * cell, 'y' :  0});
+        var endPoint = Utils.isometricToScreen({'x' : x * cell, 'y' :  count * cell});
+
+        xStart = size / 2 + startPoint.x;
+        yStart = size / 2 + startPoint.y;
+
+        xEnd = size / 2 + endPoint.x;
+        yEnd = size / 2 + endPoint.y;
+
+        ctx.moveTo(xStart, yStart);
+        ctx.lineTo(xEnd, yEnd);
+    }
+    ctx.stroke();
+
+
+    holder.addEventListener('mousemove', function(e){
+        d.innerText = JSON.stringify(Utils.screenToIsometric(e.layerX - 250, e.layerY - 250));
+    });
+
+    if (!document.addEventListener) {
+        document.body.innerHTML  = 'fuck ie';
+    }
+})()
---
+++ script/utils/math.js
@@ -0,0 +1,27 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: Медведь
+ * Date: 21.05.13
+ * Time: 3:34
+ * To change this template use File | Settings | File Templates.
+ */
+var Utils = {
+    rad : function(arg){
+        return arg * Math.PI / 180;
+    },
+
+    screenToIsometric : function(x ,y){
+        return {
+            x : (x / Math.cos(Utils.rad(30)) + y / Math.sin(Utils.rad(30))) / 2,
+            y : (y / Math.sin(Utils.rad(30)) - x / Math.cos(Utils.rad(30))) / 2
+        }
+    },
+
+    isometricToScreen : function(point){
+        return {
+            x : point.x * Math.cos(Utils.rad(30)) - point.y * Math.cos(Utils.rad(30)),
+            y : point.x * Math.sin(Utils.rad(30)) + point.y * Math.sin(Utils.rad(30))
+        }
+    }
+
+};
